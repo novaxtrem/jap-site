d8e0b5e964723613e59afc30f918ee239a20594f
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-08-15 01:50:59 -0300

    up

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const loginForm = document.getElementById("login-form");
     const loginButton = document.getElementById("login-form-submit-button");
-    const loginWithGoogle = document.getElementById("google-login-button");
+    //  const loginWithGoogle = document.getElementById("google-login-button");
 
     loginButton.addEventListener("click", (e) => {
         e.preventDefault();
@@ -16,29 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("nop");
         }
     })
-
-    loginWithGoogle.addEventListener("click", onSignIn(googleUser));
-
-    function onSignIn() {
-
-        const googleUser = gapi.auth2.getAuthInstance().currentUser.get();
-
-
-        // Useful data for your client-side scripts:
-        var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
-
-        // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
-        console.log("ID Token: " + id_token);
-        redirect(profile);
-
-    }
 
     function redirect(profile) {
         if (profile.getId() == "") {
