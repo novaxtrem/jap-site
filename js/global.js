f88819410067bdf2695f6be0bf53b35744100e44
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-08-15 02:36:28 -0300

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const autenticado = false;
-    alert("lei el file")
+    // alert("lei el file")
     if (localStorage.getItem("USERNAME") == "") {
         alert("no hay usuario")
     } else {
         autenticado = true;
     }
 
+
+
+    window.onbeforeunload = function(e) {
+        window.onunload = function() {
+            window.localStorage.isMySessionActive = "false";
+            localStorage.clear();
+        }
+        return undefined;
+    };
+
+    window.onload = function() {
+        window.localStorage.isMySessionActive = "true";
+    };
 
 });
