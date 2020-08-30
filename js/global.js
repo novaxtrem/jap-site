06f64042c58f9899ed26c6f78bdd1688607c1948
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-08-30 14:34:55 -0300

    up

--- js/global.js
+++ js/global.js
@@ -32,13 +32,13 @@ document.addEventListener("DOMContentLoaded", () => {
         //const userNavBtn = document.createElement('ul');
         htmlContentToAppend += `<nav class="site-header sticky-top py-1 bg-dark">
           <div class="container d-flex flex-column flex-md-row justify-content-between">
-              <a class="py-2 d-none d-md-inline-block" href="index.html">Inicio</a>
+              <a class="py-2 d-none d-md-inline-block" href="mainPage.html">Inicio</a>
               <a class="py-2 d-none d-md-inline-block" href="categories.html">Categorías</a>
               <a class="py-2 d-none d-md-inline-block" href="products.html">Productos</a>
               <div class="dropdown">
                   <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">` + localStorage.getItem("USERNAME") + `</button>
                   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                  <a class="dropdown-item" href="#">Mi cuenta</a>
+                  <a class="dropdown-item" href="my-profile.html">Mi cuenta</a>
                   <a class="dropdown-item" href="sell.html">Vender</a>
                   <a class="dropdown-item" href="cart.html">Mi carrito</a>
                   <a class="dropdown-item" href="logout.html">Cerrar sesion</a>
--- js/search.js
+++ js/search.js
@@ -73,11 +73,9 @@ document.addEventListener("DOMContentLoaded", () => {
             var searchField = $('#ajaxSearch').val();
             var expression = new RegExp(searchField, "i");
             $.getJSON(PRODUCTS_URL, function(data) {
-                $('#resultado').empty();
+                $('#resultado').empty(); //limpio resultados para que no se dupliquen
                 $.each(data, function(key, product) {
-
                     if (product.name.search(expression) != -1 || product.description.search(expression) != -1) {
-
                         $('#resultado').append('<li class="list-group-item link-class"><img src="' + product.imgSrc + '" height="40" width="40" class="img-thumbnail" /> ' + product.name + '<span class="text-muted">' + product.cost + " " + product.currency + '</span></li>');
                     }
                 });
@@ -85,10 +83,12 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         $('#resultado').on('click', 'li', function() {
             var click_text = $(this).text().split('|');
+            alert(click_text);
             $('#ajaxSearch').val($.trim(click_text[0]));
             $("#resultado").html('');
         });
 
+        //si cliquean fura del cuadro de busqueda borro el texto escrito y elimino los resultados
         $('body').on('click', function() {
             $('#ajaxSearch').val("");
             $('#resultado').empty();
