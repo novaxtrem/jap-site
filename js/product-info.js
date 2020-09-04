42d722f9127e793f9b941c441fc79a03aad84be1
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-09-03 23:42:48 -0300

    up

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,7 @@
 var product = {};
 var currentProductsArray = [];
 var productsCommentsArray = [];
+var productosRelacionados = [];
 
 //DIBUJO GALERIA
 function showImagesGallery(array) {
@@ -20,12 +21,21 @@ function showImagesGallery(array) {
     }
 }
 
+function getRelatedProducts(array) {
+    for (let i = 0; i < array.length; i++) {
+        productosRelacionados.push(array[i]);
+    }
+
+}
+
+
+
+
 //CARGO DESCRIPCION Y IMAGENES
 document.addEventListener("DOMContentLoaded", function(e) {
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj) {
         if (resultObj.status === "ok") {
             product = resultObj.data;
-
             let productNameHTML = document.getElementById("productName");
             let productDescriptionHTML = document.getElementById("productDescription");
             let productPriceAndCurrencyHTML = document.getElementById("productPriceAndCurrency");
@@ -34,36 +44,31 @@ document.addEventListener("DOMContentLoaded", function(e) {
             productDescriptionHTML.innerHTML = product.description;
             productPriceAndCurrencyHTML.innerHTML = product.cost + " " + product.currency;
 
-
             //Muestro las imagenes en forma de galería
             showImagesGallery(product.images);
+            getRelatedProducts(product.relatedProducts);
         }
     });
+
     //AGREGO PRODUCTOS RELACIONADOS
-    var currentCarSelected = "Chevrolet Onix Joy"; //voy a ignorar el auto que estamos mirando en los productos seleccionados
     getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        let htmlContentToAppend = "";
         if (resultObj.status === "ok") {
             currentProductsArray = resultObj.data;
-
-            let htmlContentToAppend = "";
-            for (let i = 0; i < currentProductsArray.length; i++) {
-                if (currentProductsArray[i].name == currentCarSelected) {
-                    //do nothing; then lets play Lol and take Syndra, Ahri or Sona > Epic midline / Hardcore Aggressive Support. esto no tiene relacion; I know (o_o(^_^)*_*)!!
-                } else {
-                    htmlContentToAppend += `
-                    <div class="col-sm-4 col-md-2">
-                        <div class="card-img-top">
-                            <img src="` + currentProductsArray[i].imgSrc + `"class="img-thumbnail" name="zoom" style="cursor:pointer">
-                        </div>
-                        <h4>
-                            <a href="# " class="font-weight-bold text-dark text-uppercase small ">` + currentProductsArray[i].name + `</a>
-                        </h4>
-                        <h5 class="text-warning">` + currentProductsArray[i].cost + ` ` + currentProductsArray[i].currency + `</h5>
-                    </div>`
-                    document.getElementById("relatedProductsInside").innerHTML = htmlContentToAppend;
-                }
-            }
-        }
+            for (var i = 0; i < productosRelacionados.length; i++) {
+                htmlContentToAppend += `
+                <div class="col-sm-4 col-md-2">
+                    <div class="card-img-top">
+                        <img src="` + currentProductsArray[productosRelacionados[i]].imgSrc + `"class="img-thumbnail" name="zoom" style="cursor:pointer">
+                    </div>
+                    <h4>
+                        <a href="# " class="font-weight-bold text-dark text-uppercase small ">` + currentProductsArray[productosRelacionados[i]].name + `</a>
+                    </h4>
+                    <h5 class="text-warning">` + currentProductsArray[productosRelacionados[i]].cost + ` ` + currentProductsArray[productosRelacionados[i]].currency + `</h5>
+                </div>`
+                document.getElementById("relatedProductsInside").innerHTML = htmlContentToAppend;
+            };
+        };
     });
     //AGREGO COMENTARIOS PRE-CARGADOS
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj) { //esta funcion se podria dividir en funciones mas simples que se encarguen de cargar cada una los arrays con datos
@@ -109,14 +114,6 @@ document.addEventListener("DOMContentLoaded", function(e) {
         }
         return starsToAdd;
     }
-
-    document.getElementsByTagName("input").addEventListener("click", function(e) {
-
-
-        alert($("input:radio[name=option]:checked").val());
-
-
-    });
 
 
 
