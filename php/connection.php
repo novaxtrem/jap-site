0a376f768be3a6d5b863f30c263a63fa8dab06f7
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-01 20:20:22 -0300

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -8,7 +8,9 @@ $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
+$mensaje ="cosopum";
   die("Connection failed: " . $conn->connect_error);
+  echo "<script type='text/javascript'>alert('$mensaje');</script>";
 }
 
 echo "<script type='text/javascript'>alert('$conn');</script>";
