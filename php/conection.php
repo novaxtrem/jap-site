fdbc1bc2945ad7eba0ffcd8f53c73c5f5c16ecfe
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-01 20:00:23 -0300

    Update conection.php

--- php/conection.php
+++ php/conection.php
@@ -10,5 +10,7 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+echo "<script type='text/javascript'>alert('$conn');</script>";
 echo "Connected successfully";
 ?>
