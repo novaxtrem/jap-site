32d1c823233f3c10d5df867f17e1f54d10cc6e8d
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-01 20:12:32 -0300

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,16 @@
 <?php
-alert("Hello World");
+$servername = "remotemysql.com:3306";
+$username = "nN3gpTO4n0";
+$password = "mOlXuDZFaT";
 
-function alert($msg) {
-    echo "<script type='text/javascript'>alert('$msg');</script>";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+echo "<script type='text/javascript'>alert('$conn');</script>";
+echo "Connected successfully";
 ?>
