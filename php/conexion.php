b57f4a2390f036fe5fdb9685833585af05697c47
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-03 20:45:16 -0300

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,16 @@
-<?php
-$host="remotemysql.com";
-$user="nN3gpTO4n0";
-$password="mOlXuDZFaT";
 
-$link = mysql_connect ($host, $user, $password) or die ("<center>No se puede conectar con la base de datos\n</center>\n");
-?>
+
+
+<?php
+         $dbhost = 'remotemysql.com:3306';
+         $dbuser = 'nN3gpTO4n0';
+         $dbpass = 'mOlXuDZFaT';
+         $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+         
+         if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+            die ("<center>No se puede conectar con la base de datos\n</center>\n");
+         }
+         echo 'Connected successfully';
+         mysqli_close($conn);
+      ?>
