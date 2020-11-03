b78774eb2c4285d9f45f604b539e32b42f2dc4fe
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-03 13:31:50 -0300

    Update consulta.php

--- php/consulta.php
+++ php/consulta.php
@@ -1,9 +1,9 @@
 <?php
 //Configuracion de la conexion a base de datos
-$bd_host = "localhost"; 
-$bd_usuario = "root"; 
-$bd_password = ""; 
-$bd_base = "ribosomatic"; 
+$bd_host = "remotemysql.com:3306"; 
+$bd_usuario = "nN3gpTO4n0"; 
+$bd_password = "mOlXuDZFaT"; 
+$bd_base = "nN3gpTO4n0"; 
 $con = mysql_connect($bd_host, $bd_usuario, $bd_password); 
 mysql_select_db($bd_base, $con); 
 //consulta todos los empleados
@@ -13,4 +13,3 @@ echo "</p>Nombres - Departamento - Sueldo</p> \n";
 while($row = mysql_fetch_array($sql)){
  echo "<p>".$row['nombres']." - ".$row['departamento']." - ".$row['sueldo']."</p> \n";
 }
-?>
