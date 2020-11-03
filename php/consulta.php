31751790fddbb6f5398973250ef531d41b655279
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-03 13:32:53 -0300

    Update consulta.php

--- php/consulta.php
+++ php/consulta.php
@@ -1,15 +1,15 @@
 <?php
 //Configuracion de la conexion a base de datos
-$bd_host = "remotemysql.com:3306"; 
-$bd_usuario = "nN3gpTO4n0"; 
-$bd_password = "mOlXuDZFaT"; 
-$bd_base = "nN3gpTO4n0"; 
-$con = mysql_connect($bd_host, $bd_usuario, $bd_password); 
-mysql_select_db($bd_base, $con); 
+$bd_host = "remotemysql.com:3306";
+$bd_usuario = "nN3gpTO4n0";
+$bd_password = "mOlXuDZFaT";
+$bd_base = "nN3gpTO4n0";
+$con = mysql_connect($bd_host, $bd_usuario, $bd_password);
+mysql_select_db($bd_base, $con);
 //consulta todos los empleados
-$sql=mysql_query("SELECT * FROM empleados",$con);
+$sql = mysql_query("SELECT * FROM empleados", $con);
 //muestra los datos consultados
 echo "</p>Nombres - Departamento - Sueldo</p> \n";
-while($row = mysql_fetch_array($sql)){
- echo "<p>".$row['nombres']." - ".$row['departamento']." - ".$row['sueldo']."</p> \n";
+while ($row = mysql_fetch_array($sql)) {
+    echo "<p>" . $row['nombres'] . " - " . $row['departamento'] . " - " . $row['sueldo'] . "</p> \n";
 }
