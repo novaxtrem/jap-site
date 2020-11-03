1977e5a9ba167176508efaffa4d72a1400439b3a
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-03 10:11:55 -0300

    up

--- js/my-profile.js
+++ js/my-profile.js
@@ -3,6 +3,28 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
 
 
+    $('#getUser').on('click', function() {
+        var user_id = $('#user_id').val();
+        $.ajax({
+            type: 'POST',
+            url: 'getData.php',
+            dataType: "json",
+            data: { user_id: user_id },
+            success: function(data) {
+                if (data.status == 'ok') {
+                    $('#userName').text(data.result.name);
+                    $('#userEmail').text(data.result.email);
+                    $('#userPhone').text(data.result.phone);
+                    $('#userCreated').text(data.result.created);
+                    $('.user-content').slideDown();
+                } else {
+                    $('.user-content').slideUp();
+                    alert("User not found...");
+                }
+            }
+        });
+    });
+
     $('#test').click(function() {
 
 
