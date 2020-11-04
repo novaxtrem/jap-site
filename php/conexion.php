09def5b44e32d66a1ffcadf3f93976d36d45e649
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-03 21:18:58 -0300

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -2,17 +2,16 @@
 
 
 <?php
-         $dbhost = 'remotemysql.com:3306';
-         $dbuser = 'nN3gpTO4n0';
-         $dbpass = 'mOlXuDZFaT';
-         $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
-         
-         if(! $conn ) {
-            die('Could not connect: ' . mysqli_error());
-           
-         } else {
-             echo 'Connected successfully';
-         }
-         
-         mysqli_close($conn);
-      ?>
+$dbhost = 'remotemysql.com:3306';
+$dbuser = 'nN3gpTO4n0';
+$dbpass = 'mOlXuDZFaT';
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+
+if (!$conn) {
+    die('Could not connect: ' . mysqli_error());
+} else {
+    echo 'Connected successfully'. $conn ;
+}
+
+mysqli_close($conn);
+?>
