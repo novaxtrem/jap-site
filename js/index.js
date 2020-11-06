495b1ba47775d2d95d75ea71a9b5bfb1c2926c03
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-06 17:28:19 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,63 +5,65 @@ document.addEventListener("DOMContentLoaded", () => {
     //  const loginWithGoogle = document.getElementById("google-login-button");
 
     loginButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        const username = loginForm.userEmail.value;
-        const password = loginForm.pass.value;
+            e.preventDefault();
+            const username = loginForm.userEmail.value;
+            const password = loginForm.pass.value;
 
-        return $.ajax({
-            url: GET_ALL_USER_PHP_RESPONSE_JSON_LINK + "?username=" + username + "&password=" + password, //I MAKE MY OWN RULES; ONLINE FREE HOSTING WITH PHP + EXTERNAL REMOTE DB (FREE ALSO)
-            type: "GET", // SENDING A USER AND PASSWORD WITHOUT HASH...SAVING A IMAGE ON A DB ON BASE64...OUTSTANDING (SARCASTIC)
-            dataType: 'json',
-            async: false,
-            // data: { field1: username },
-            success: function(data) {
+            return $.ajax({
+                    url: GET_ALL_USER_PHP_RESPONSE_JSON_LINK + "?username=" + username + "&password=" + password, //I MAKE MY OWN RULES; ONLINE FREE HOSTING WITH PHP + EXTERNAL REMOTE DB (FREE ALSO)
+                    type: "GET", // SENDING A USER AND PASSWORD WITHOUT HASH...SAVING A IMAGE ON A DB ON BASE64...OUTSTANDING (SARCASTIC)
+                    dataType: 'json',
+                    async: false,
+                    // data: { field1: username },
+                    success: function(data) {
 
-                if (!$.trim(data)) {
-                    alert("Error con el usuario y/o la contraseña");
-                } else {
+                        if (!$.trim(data)) {
+                            alert("Error con el usuario y/o la contraseña");
+                        } else {
 
-                    alert(data[0].id);
-                    alert(data[0].last_name);
-                    window.location.replace("mainPage.html");
-                    var decodedString = atob(data[0].image_profile);
-
-                    alert("<img src=" + decodedString + ">");
-
-                    /*
-                                        // Encode the String
-                                        var encodedString = btoa(string);
-                                        console.log(encodedString); // Outputs: "SGVsbG8gV29ybGQh"
-                    */
-                    console.log(decodedString); // Outputs: "Hello World!"
-
-                }
+                            alert(data[0].id);
+                            alert(data[0].last_name);
+                        }
+                        //
+                        var decodedString = atob(data[0].image_profile);
+                        alert("<img src=" + decodedString + ">");
+                        window.location.replace("mainPage.html");
 
 
-                // for (var i = 0; i < data.length; i++) {
+                        /*
+                                            // Encode the String
+                                            var encodedString = btoa(string);
+                                            console.log(encodedString); // Outputs: "SGVsbG8gV29ybGQh"
+                        */
+                        console.log(decodedString); // Outputs: "Hello World!"
 
-                /* if (username == data[i].email) {
-                     if (password == data[i].password) {
-                         alert(data[i].name + " " + data[i].last_name);
-                         //
-                         localStorage.setItem('NOMBRE', data[i].name);
-                         localStorage.setItem('APELLIDO', data[i].last_name);
-                         localStorage.setItem('USER_EMAIL', username);
-                         localStorage.setItem('TELEFONO', data[i].phone_num);
-                         localStorage.setItem('EDAD', data[i].age);
-                         //
-                         localStorage.setItem("USER_PROFILE_IMG", "https://image.flaticon.com/icons/svg/244/244341.svg");
-                         window.location.href = "mainPage.html";
+                    }
+
+
+                    // for (var i = 0; i < data.length; i++) {
+
+                    /* if (username == data[i].email) {
+                         if (password == data[i].password) {
+                             alert(data[i].name + " " + data[i].last_name);
+                             //
+                             localStorage.setItem('NOMBRE', data[i].name);
+                             localStorage.setItem('APELLIDO', data[i].last_name);
+                             localStorage.setItem('USER_EMAIL', username);
+                             localStorage.setItem('TELEFONO', data[i].phone_num);
+                             localStorage.setItem('EDAD', data[i].age);
+                             //
+                             localStorage.setItem("USER_PROFILE_IMG", "https://image.flaticon.com/icons/svg/244/244341.svg");
+                             window.location.href = "mainPage.html";
+                         }
                      }
-                 }
-                 */
-                /*var newArticle = new Article(data.articles[i].name, data.articles[i].count, data.articles[i].unitCost, data.articles[i].currency, data.articles[i].src); //CREO EL OBJETO
-                articleList.push(newArticle); //AGREGO EL OBJETO AL ARRAYLIST
-                */
+                     */
+                    /*var newArticle = new Article(data.articles[i].name, data.articles[i].count, data.articles[i].unitCost, data.articles[i].currency, data.articles[i].src); //CREO EL OBJETO
+                    articleList.push(newArticle); //AGREGO EL OBJETO AL ARRAYLIST
+                    */
 
-                //  }
-            }
-        });
+                    //  }
+                }
+            });
         /*
         if (username == "user@net.com" && password == "1234") {
             localStorage.setItem('NOMBRE', "Jonh");
