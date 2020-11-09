6e98d41bf12e93f26bb36f9666d9126285b703fb
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-11-09 00:13:34 -0300

    Update logout.js

--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,19 @@
 alert("Gracias por tu visita");
 localStorage.clear();
 profile = null;
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function() {
+        alert('User signed out.');
+        window.location.href = "index.html";
+        localStorage.clear();
+    });
+}
+
+function onLoad() {
+    gapi.load('auth2', function() {
+        gapi.auth2.init();
+    });
+}
 window.location.href = "index.html";
